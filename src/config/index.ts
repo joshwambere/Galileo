74fb86feac49b161dd0608b3960b6e545f148da7
johnson <johnsondusabe@gmail.com>
2024-04-22 17:06:23 +0200

    fix env variable not loading in app

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,5 @@
 import { config } from 'dotenv';
-config({ path: `.env.${process.env.NODE_ENV || 'development'}` });
+config({ path: `.env.${process.env.NODE_ENV || 'production'}` });
 
 export const CREDENTIALS = process.env.CREDENTIALS === 'true';
 
